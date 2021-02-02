b3f8ca00d28c45810fa609b413c3c6a883b9606e
Paul-Vincent Roll <paviro@me.com>
2021-02-03 00:22:46 +0100

    Moved data to config + other improvements

--- plugin.php
+++ plugin.php
@@ -20,102 +20,105 @@
     // Content 
     
     const CALENDAR_PLACEHOLDER = "[calendar]";
-
-    const CALENDAR_DATES = "CalendarDates";
     
-    const CALENDAR_DATA = [
-      ["title" => "Awesome Meeting",
-       "date" => "01/01/2021 15:16:17",
-       "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue condimentum dui, ut rhoncus erat posuere non. Ut dignissim in sem eget suscipit.",
-       "image" => "https://picsum.photos/150",
-       "tags" => [
-          "German",
-          "Meeting",
-          "BBB"
-        ]],
-      ["title" => "Awesome Meeting",
-        "date" => "02/02/2021 15:16:17",
-        "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue condimentum dui, ut rhoncus erat posuere non. Ut dignissim in sem eget suscipit.",
-        "image" => "https://picsum.photos/150",
-        "tags" => [
-          "German",
-          "Meeting",
-          "BBB"
-        ]],
-      ["title" => "Awesome Meeting",
-        "date" => "05/02/2021 15:16:17",
-        "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue condimentum dui, ut rhoncus erat posuere non. Ut dignissim in sem eget suscipit.",
-        "image" => "https://picsum.photos/150",
-        "tags" => [
-          "German",
-          "Meeting",
-          "BBB"
-        ]],
-      ["title" => "Awesome Meeting",
-        "date" => "20/02/2021 15:16:17",
-        "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue condimentum dui, ut rhoncus erat posuere non. Ut dignissim in sem eget suscipit.",
-        "image" => "https://picsum.photos/150",
-        "tags" => [
-          "German",
-          "Meeting",
-          "BBB"
-        ]]
-    ];
+    // CONSTANTS
+    // config 
+    const CALENDAR_DATA = "CALENDAR_DATA";
+    const CALENDAR_NO_ENTRIES_TEXT = "CALENDAR_NO_ENTRIES_TEXT";
+    const CALENDAR_BUTTON_SHOW_PAST_TEXT = "CALENDAR_BUTTON_SHOW_PAST_TEXT";
+    const CALENDAR_BUTTON_HIDE_PAST_TEXT = "CALENDAR_BUTTON_HIDE_PAST_TEXT";
+    const CALENDAR_TIMEZONE = "CALENDAR_TIMEZONE";
   
+    protected static function configure() {
+       Plugins::preset(static::CALENDAR_DATA, null);
+       Plugins::preset(static::CALENDAR_NO_ENTRIES_TEXT, "No entries.");
+       Plugins::preset(static::CALENDAR_BUTTON_SHOW_PAST_TEXT, "Show past");
+       Plugins::preset(static::CALENDAR_BUTTON_HIDE_PAST_TEXT, "Hide past");
+       Plugins::preset(static::CALENDAR_TIMEZONE, new DateTimeZone('Europe/Berlin'));
+    }
+    
+    public static function addTimezoneConversion() {
+      print(fhtml("<!-- Timezone conversion -->").NL);
+      print(fhtml("<script src='".path2uri(__DIR__."/lib/moment.js")."'></script>").NL);
+      print(fhtml("<script>").NL);
+      print(fhtml("moment.locale(navigator.userLanguage || navigator.language);").NL);
+      print(fhtml("var dates = document.getElementsByClassName(\"date\");").NL);
+      print(fhtml("for (date of dates) {
+                     time = moment(date.dataset.time);
+                     date.innerHTML = time.format('LLLL');
+                 }".NL));
+      print(fhtml("</script>").NL);
+      
+    }
     
     protected static function getCalendar($item) {
+      // preset plugin configuration
+      static::configure();
+      
       $result = value($item, CONTENT);
-      $calendarHTML = String;
       $eventsDisplayed = 0;
       
       if (is_string($result)) {
-        $calendarData = json_decode(value($item, self::CALENDAR_DATES), TRUE);
-        
-        if ($calendarData) {
+        if (Plugins::get(static::CALENDAR_DATA)) {
+          $entries = Plugins::get(static::CALENDAR_DATA);
+          usort($entries, function($a, $b) {
+            return strtotime($a["date"]) - strtotime($b["date"]);
+          });
+          
           $calendarHTML = fhtml("<div class=\"row justify-content-center %s\">".NL,
             "text-left");
-          foreach ($calendarData as $ENTRY) {
-            $date = DateTime::createFromFormat('d/m/Y H:i:s', $ENTRY["date"]);
-            if (new DateTime() < $date or (bool)$_GET['showOld'] == true) {
+          foreach ($entries as $entry) {
+            
+            $timezone = new DateTimeZone($entry["timezone"]);
+            $date = DateTime::createFromFormat('d/m/Y H:i:s', $entry["date"], $timezone);
+            $date->setTimezone(Plugins::get(static::CALENDAR_TIMEZONE));
+            
+            if (new DateTime() < $date or isset($_GET['showPast']) && (bool)$_GET['showPast'] == true) {
               $eventsDisplayed +=1; 
               
               $tags = "";
               
-              foreach ($ENTRY["tags"] as $tag) {
+              foreach ($entry["tags"] as $tag) {
                 $tags = $tags.fhtml("<span class=\"calendarTag\">%s</span>".NL, $tag);
               }
               
-              $calendarHTML = $calendarHTML.fhtml("<div class=\"col-xxxl-3 col-xxl-4 col-xl-5 col-lg-9 col-md-10 col-sm-10 col-10 mx-5 mb-3 calendarItem\">".NL);
+              $calendarHTML .= fhtml("<div class=\"col-xxxl-3 col-xxl-4 col-xl-5 col-lg-9 col-md-10 col-sm-10 col-10 mx-5 mb-3 calendarItem\">".NL);
               
-              $calendarHTML = $calendarHTML.fhtml("
+              $calendarHTML .= fhtml("
                 <div class=\"image\">
                   <img src=\"%s\">
-                </div>".NL, $ENTRY["image"]);
+                </div>".NL, $entry["image"]);
               
-              $calendarHTML = $calendarHTML.fhtml("
+              $calendarHTML .= fhtml("
                 <div class=\"summary\">
                   <a href=\"%s\"><h4 class=\"text-white header\">%s</h4></a>
-                  <p class=\"text-white date\">%s <small>UTC +1</small></p>
-                  <p class=\"text-white text\">%s</p>".NL, $ENTRY["link"], $ENTRY["title"], date_format($date,"jS F Y H:i"), $ENTRY["summary"]);
+                  <p class=\"text-white date\" data-time=\"%s\">%s <small>UTC +1</small></p>
+                  <p class=\"text-white text\">%s</p>".NL, $entry["link"], $entry["title"], $date->format('c'), date_format($date,"jS F Y H:i"), $entry["summary"]);
               
-              $calendarHTML = $calendarHTML.$tags;
+              $calendarHTML .= $tags;
               
-              $calendarHTML = $calendarHTML.fhtml("</div>".NL);
+              $calendarHTML .= fhtml("</div>".NL);
               
-              $calendarHTML = $calendarHTML.fhtml("</div>".NL);
+              $calendarHTML .= fhtml("</div>".NL);
             }
           }
-          
-          if ($eventsDisplayed == 0) {
-            $calendarHTML = fhtml("At the moment, there do not seem to be any events.");
-          }
-          
-          $calendarHTML = $calendarHTML.fhtml("</div>");
-        } else {
-          $calendarHTML = fhtml("At the moment, there do not seem to be any events.");
+          $calendarHTML .= fhtml("</div>".NL);
+        } 
+        
+        if ($eventsDisplayed == 0) {
+          $calendarHTML = fhtml("<p>".Plugins::get(static::CALENDAR_NO_ENTRIES_TEXT)."</p>".NL);
         }
         
+        if (count(Plugins::get(static::CALENDAR_DATA)) != $eventsDisplayed) {
+          $calendarHTML .= fhtml("<a class=\"btn btn-primary btn-l\" href=\"?showPast=1\">%s</a>".NL, Plugins::get(static::CALENDAR_BUTTON_SHOW_PAST_TEXT));
+        } elseif ((bool)$_GET['showPast']) {
+          $calendarHTML .= fhtml("<a class=\"btn btn-primary btn-l\" href=\"%s\">%s</a>".NL, rtrim(explode("?", $_SERVER['REQUEST_URI'])[0],"/"), Plugins::get(static::CALENDAR_BUTTON_HIDE_PAST_TEXT));
+        }
+        
+    
+        
         $result = str_ireplace(static::CALENDAR_PLACEHOLDER, $calendarHTML, $result);
+        
         }
 
       return $result;
@@ -150,3 +153,4 @@
 
   // register plugin
   Plugins::register(CalendarPlugin::class, "plugin", FILTER_CONTENT);
+  Plugins::register(CalendarPlugin::class, "addTimezoneConversion", AFTER_BODY);
